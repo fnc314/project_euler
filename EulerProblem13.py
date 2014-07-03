de54193801348f1589d37b5d11df18b8f37ba4bc
Franco N. Colaizzi <fnc314@gmail.com>
2014-07-03 12:32:57 -0400

    slightly modified and improved answer to #13 so exactly first ten digits were returned as opposed to entire computation

--- EulerProblem13.py
+++ EulerProblem13.py
@@ -1,17 +1,24 @@
 def ep13():
-  total = 0
+  total_integer = 0
 
   with open('EulerProblem13.txt', 'r') as f:
     for line in f:
       num = int(line)
-      total += num
+      total_integer += num
 
   # for string in ten_digit_strings:
   #   number = int(string)
   #   total += number
   #
-  return total
+  return first_ten_digits(total_integer)
+
+
+def first_ten_digits(total_integer):
+  total_string = str(total_integer)
+  return int(total_string[0:10])
+
 
 print ep13()
 
 # CORRECT
+# Slightly modified for improved output
