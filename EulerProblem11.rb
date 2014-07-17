762a51c37a60425381532b9775119f2ef8cdd82f
Franco N. Colaizzi <fnc314@gmail.com>
2014-07-17 14:40:48 -0400

    attempted abstraction of multiplication through grid to try and reattack problem with better tools

--- EulerProblem11.rb
+++ EulerProblem11.rb
@@ -12,12 +12,39 @@ def fileReader()
   return grid
 end
 
-# puts "*" * 10
-# print fileReader()
-# puts "*" * 10
-
 # grid is an array (the array returned by fileReader() above)
 # grid is 20 x 20 = 400 integers
+
+# Create function that takes in `grid`, `r` (int), `c` (int), and `dir` (string representing direction "U","UR","R","DR","D","DL","L","UL") and calculates the required product => Returns value => function should be called `products.push(function(grid,r,c,dir))` within `four_term_products(grid)`
+
+def function(grid,r,c,dir)
+  '''
+  grid = array of arrays } 20 arrays, 20 elements each, 400 element grid
+  r, c = integers representing row and column indecies 0 <= r,c < 20
+  dir = string for direction on the grid to travel to gather terms for product
+  '''
+  first_term = grid[r][c]
+
+  case dir
+
+    when "U"
+      return (first_term * grid[r-1][c] * grid[r-2][c] * grid[r-3][c])
+    when "UR"
+      return (first_term * grid[r-1][c+1] * grid[r-2][c+2] * grid[r-3][c+3])
+    when "R"
+      return (first_term * grid[r][c+1] * grid[r][c+2] * grid[r][c+3])
+    when "DR"
+      return (first_term * grid[r+1][c+1] * grid[r+2][c+2] * grid[r+3][c+3])
+    when "D"
+      return (first_term * grid[r+1][c] * grid[r+2][c] * grid[r+3][c])
+    when "DL"
+      return (first_term * grid[r+1][c-1] * grid[r+2][c-2] * grid[r+3][c-3])
+    when "L"
+      return (first_term * grid[r][c-1] * grid[r][c-2] * grid[r][c-3])
+    when "UL"
+      return (first_term * grid[r-1][c-1] * grid[r-2][c-1] * grid[r-3][c-1])
+  end
+end
 
 def four_term_products(grid)
 
