9832a8404dc058dfc9a812d75b20c9e4e747fc4f
Franco N. Colaizzi <fnc314@gmail.com>
2014-07-18 11:37:09 -0400

    implemented method to calculate products...working out cases

--- EulerProblem11.rb
+++ EulerProblem11.rb
@@ -12,12 +12,12 @@ def fileReader()
   return grid
 end
 
-# grid is an array (the array returned by fileReader() above)
+# grid is an array of arrays (returned by fileReader() above)
 # grid is 20 x 20 = 400 integers
 
-# Create function that takes in `grid`, `r` (int), `c` (int), and `dir` (string representing direction "U","UR","R","DR","D","DL","L","UL") and calculates the required product => Returns value => function should be called `products.push(function(grid,r,c,dir))` within `four_term_products(grid)`
+# Create function `multiplier(grid,r,c,dir)` that takes in `grid`, `r` (int), `c` (int), and `dir` (string representing direction "U","UR","R","DR","D","DL","L","UL") and calculates the required product => Returns value => function should be called `products.push(multiplier(grid,r,c,dir))` within `four_term_products(grid)`
 
-def function(grid,r,c,dir)
+def multiplier(grid,r,c,dir)
   '''
   grid = array of arrays >> 20 arrays, 20 elements each, 400 element grid
   r, c = integers representing row and column indecies 0 <= r,c < 20
@@ -43,53 +43,45 @@ def function(grid,r,c,dir)
       return (first_term * grid[r][c-1] * grid[r][c-2] * grid[r][c-3])
     when "UL"
       return (first_term * grid[r-1][c-1] * grid[r-2][c-1] * grid[r-3][c-1])
+
   end
+
 end
 
 def four_term_products(grid)
 
   products = Array.new()
+  num_rows = grid.length
+  num_cols = grid[0].length
+  # any element in `grid` should suffice >> assuming square grid
+
 
   r = 0 # r for row
-  while r < 20 do
+  while r < num_rows do
     c = 0 # c for column
-    while c < 20 do
+    while c < num_cols do
 
-      if r < 17 #after here it's impossible to calculate downward products
+      if r < 17 # r = 0, 1, 2, 3, ..., 14, 15, 16
 
-        if c < 3
-          # DOWN product
-          products.push(grid[r][c] * grid[r+1][c] * grid[r+2][c] * grid[r+3][c])
-          # DOWN-RIGHT product
-          products.push(grid[r][c] * grid[r+1][c+1] * grid[r+2][c+2] * grid[r+3][c+3])
-          # RIGHT product
-          products.push(grid[r][c] * grid[r][c+1] * grid[r][c+2] * grid[r][c+3])
+        if c < 3 # c = 0, 1, 2
+          products.push(multiplier(grid,r,c,"D")) # DOWN product
+          products.push(multiplier(grid,r,c,"DR")) # DOWN-RIGHT product
+          products.push(multiplier(grid,r,c,"R")) # RIGHT product
+        elsif c > 2 && c < 17
+          products.push(multiplier(grid,r,c,"L")) # LEFT product
+          products.push(multiplier(grid,r,c,"DL")) # DOWN-LEFT product
+          products.push(multiplier(grid,r,c,"D")) # DOWN product
+          products.push(multiplier(grid,r,c,"DR")) # DOWN-RIGHT product
+          products.push(multiplier(grid,r,c,"R")) # Right product
         elsif c > 16
-          # LEFT product
-          products.push(grid[r][c] * grid[r][c-1] * grid[r][c-2] * grid[r][c-3])
-          # DOWN-LEFT product
-          products.push(grid[r][c] * grid[r-1][c-1] * grid[r-2][c-2] * grid[r-3][c-3])
-          # DOWN product
-          products.push(grid[r][c] * grid[r+1][c] * grid[r+2][c] * grid[r+3][c])
-        else
-          # LEFT product
-          products.push(grid[r][c] * grid[r][c-1] * grid[r][c-2] * grid[r][c-3])
-          # DOWN-LEFT product
-          products.push(grid[r][c] * grid[r-1][c-1] * grid[r-2][c-2] * grid[r-3][c-3])
-          # DOWN product
-          products.push(grid[r][c] * grid[r+1][c] * grid[r+2][c] * grid[r+3][c])
-          # DOWN-RIGHT product
-          products.push(grid[r][c] * grid[r+1][c+1] * grid[r+2][c+2] * grid[r+3][c+3])
-          # RIGHT product
-          products.push(grid[r][c] * grid[r][c+1] * grid[r][c+2] * grid[r][c+3])
+          products.push(multiplier(grid,r,c,"L")) # LEFT product
+          products.push(multiplier(grid,r,c,"DL")) # DOWN-LEFT product
+          products.push(multiplier(grid,r,c,"D")) # DOWN product
         end
 
-      else
-        if c < 16 # The last 3 rows can only compute the RIGHT product until reaching c = 17, at which case nil is returned by grid[r][c+3]
-          # RIGHT product
-          products.push(grid[r][c] * grid[r][c+1] * grid[r][c+2] * grid[r][c+3])
-        end
-      end
+      elsif r > 16 # r = 17, 18, 19 (only right products)
+
+        if c
 
       c += 1
     end
