b8ba0e141be596f0f497ff8b54ed1962af078d47
Franco N. Colaizzi <fnc314@gmail.com>
2014-07-19 02:02:25 -0400

    leanred RegEx in Python (mostly)...can attempt to build solution that obtains the given example results

--- EulerProblem474.py
+++ EulerProblem474.py
@@ -13,8 +13,12 @@ def factors_of(n):
 
 def factor_compare(n,d):
   factors = factors_of(n)
-  factor_last_digits = str(d)
+  factor_last_digits = re.compile(str(d))
 
   for i in factors:
     string_i = str(i)
-    
+
+
+# First use `expr = re.compile(str(d))` to set up `d` to be the RegEx used when examining factors
+# Second use `expr.match(str(i))` to test for existance of `d` in factor
+# Third use a dummy variable to keep count and return its final value => Check with provided examples!
